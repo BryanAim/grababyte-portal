1b8d4348cf6b5aad7bd4674b0452f3e9a0f110c7
Isale <isale.bryan@gmail.com>
2020-02-21 21:31:22 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+
+/**
+ * Server Activation
+ */
