ef5cc4fe4977dac655e48419a9f7a582617add8c
Isale <isale.bryan@gmail.com>
2020-02-22 10:57:17 +0300

    Add pug templating engine and configuration

--- index.js
+++ index.js
@@ -9,15 +9,26 @@ const path = require("path");
 /**
  * App Variables
  */
+const app = express();
+const port = process.env.PORT || "8000";
 
 /**
- *  App Configuration
+ *  App Configuration ie configuring express to use Pug as the view template engine
  */
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine", "pug");
 
 /**
  * Routes Definitions
  */
+app.get('/', (req, res) => {
+  res.render('index', {title: "Home"});
+});
 
 /**
  * Server Activation
  */
+app.listen(port, ()=> {
+  console.log(`Listening to requests on http://localhost:${port}`);
+  
+});
